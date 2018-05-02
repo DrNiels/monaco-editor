6585204c938e26534b16ab72197059c0cfc2bbe0
Niels Thole <niels.thole@gmail.com>
2018-05-02 11:59:21 +0200

    Now function clearHoverProviders to remove all existing hover providers

--- monaco-editor-iframe.js
+++ monaco-editor-iframe.js
@@ -54,6 +54,13 @@
             event: 'registerHoverProvider',
             args: [languageId, provider]
           });
+        },
+        clearHoverProviders: () => {
+          this.postMessage({
+            path: ['monaco', 'languages'],
+            event: 'clearHoverProviders',
+            args: []
+          });
         }
       };
     }
@@ -270,6 +277,7 @@
             var editorReference = "${opts.editorReference}"
             var proxy = {};
             var queue = [];
+            var hoverProviders = [];
             
             require.config({
               paths: {vs: "${libPath}"}
@@ -335,6 +343,12 @@
                 proxy.editor.getModel().setValue(args[0]);
                 return;
               }
+              if (event === 'clearHoverProviders') {
+                for (let hoverProvider of hoverProviders) {
+                  hoverProvider.dispose();
+                }
+                return;
+              }
               if(event === 'highlightLine') {
                 proxy.editor.decorationList = [];
                 var debugInfo = args[0];
@@ -386,7 +400,10 @@
               try {
                 let cmd = proxy;
                 path.forEach(k => { cmd = cmd[k]; });
-                cmd[event](...args);
+                let disposable = cmd[event](...args);
+                if (event === 'registerHoverProvider') {
+                  hoverProviders.push(disposable);
+                }
               } catch (err) {
                 console.error(err);
               }
